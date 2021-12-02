d1299fa0cc3b38b03e24813451dd9da62fc44523
SKIEH1125 <>
2021-12-02 22:29:35 +0000

    Added Comments

--- main.py
+++ main.py
@@ -1,22 +1,24 @@
 import discord
 import os
 
-client = discord.Client()
+client = discord.Client() # Creates a Discord Client
 
 @client.event
 async def on_ready(): # Happens when the bot goes online
   print('Logged in as {0.user}'.format(client))
 
 @client.event
-async def on_message(message):
+async def on_message(message): # Happens each time a message is sent in a text channel.
   if message.author == client.user: # Keeps the bot from responding to its own messages.
     return
   
   # Additional Bot behaviors can be added below!
 
+  # Ping, Pong
   if message.content.startswith('ping!'):
     await message.channel.send('pong!')
   
+  # Reverse Text
   if message.content.startswith('!reverse '):
     await message.channel.send((message.content)[9 : len(message.content)][::-1])
 
